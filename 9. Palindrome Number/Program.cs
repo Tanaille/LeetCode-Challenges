879ceeba139a16307a9096fcf7424a02e44c2da5
Ruan Marx <ruanmarx2@gmail.com>
2023-01-15 10:48:45 +0200

    Finished challenge [9. Palindrome Number]

---
+++ 9. Palindrome Number/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _9._Palindrome_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution.IsPalindrome(2552));
+        }
+    }
+
+    public class Solution
+    {
+        public static bool IsPalindrome(int x)
+        {
+            bool isPalindrome = true;
+            string number = x.ToString();
+
+            for (int i = 0, j = (number.Length - 1); i < number.Length; i++)
+            {
+                if (number[i] != number[j])
+                    isPalindrome = false;
+
+                j--;
+            }
+
+            return isPalindrome;
+        }
+    }
+}
